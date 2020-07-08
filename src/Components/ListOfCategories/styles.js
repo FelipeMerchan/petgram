c901269eb81c67439a670fd6a0b016f103329fb1
Felipe Merchán <felipe-sc@hotmail.es>
2020-07-07 23:06:20 -0500

    Modulo 4 - clases: ¿Que son los Hooks? y useEffect: limpiando eventos

--- src/Components/ListOfCategories/styles.js
+++ src/Components/ListOfCategories/styles.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animations'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+  &.fixed {
+    position: fixed;
+    top: -20px;
+    right: 0%;
+    left: 0;
+    z-index: 1;
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 5px;
+    border-radius: 60px;
+    ${fadeIn()}
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    background: #ffffff;
+    transform: scale(.5);
+  }
 `
 
 export const Item = styled.li`
