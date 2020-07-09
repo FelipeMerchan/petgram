a549c9cb2782a25840a5387bfe455e07c745f5a5
Felipe Merchán <felipe-sc@hotmail.es>
2020-07-08 19:30:31 -0500

    Modulo 4 - clase: useCategoriesData

--- src/Components/ListOfCategories/styles.js
+++ src/Components/ListOfCategories/styles.js
@@ -1,25 +1,27 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { fadeIn } from '../../styles/animations'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-  &.fixed {
-    position: fixed;
-    top: -20px;
-    right: 0%;
-    left: 0;
-    z-index: 1;
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 5px;
-    border-radius: 60px;
-    ${fadeIn()}
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
-    background: #ffffff;
-    transform: scale(.5);
-  }
+  ${props => props.fixed && css`
+    {
+      position: fixed;
+      top: -20px;
+      right: 0%;
+      left: 0;
+      z-index: 1;
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 5px;
+      border-radius: 60px;
+      ${fadeIn()}
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+      background: #ffffff;
+      transform: scale(.5);
+    }
+  `}
 `
 
 export const Item = styled.li`
