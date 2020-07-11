a2e1fbdcd3fd0466c09c8ca3294748aec3f4b970
Felipe Merchán <felipe-sc@hotmail.es>
2020-07-11 16:21:50 -0500

    Modulo 5 - clase: Parametros para un query con GraphQL

--- src/Components/ListOfPhotoCards/index.js
+++ src/Components/ListOfPhotoCards/index.js
@@ -1,22 +1,7 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
 
-const withPhotos = graphql(gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`)
-
-const ListOfPhotoCardsComponent = (
+export const ListOfPhotoCardsComponent = (
   { data: { photos = [] } }
   = {}) => {
   return (
@@ -25,5 +10,3 @@ const ListOfPhotoCardsComponent = (
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
