556c62520605e6d5d3ce1dde89a82432e7fe09e9
Felipe Merchán <felipe-sc@hotmail.es>
2020-07-12 14:23:16 -0500

    Modulo 6 -clase: Creando la pagina Detail

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import { Router } from '@reach/router'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { Detail } from './pages/Detail'
 import { Logo } from './components/Logo'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-            </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </div>
   )
 }
