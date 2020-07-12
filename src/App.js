d4d69bbc4c1a6786c750734fa44ba6f5a1811f65
Felipe Merchán <felipe-sc@hotmail.es>
2020-07-12 15:25:25 -0500

    Modulo 6 - clase: Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NavBar } from './components/NavBar'
 import { Logo } from './components/Logo'
 
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 export const App = () => {
   return (
     <div>
@@ -17,6 +23,20 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+                </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+                </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
