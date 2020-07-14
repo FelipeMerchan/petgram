d35711068884bced8eb8571105b620ddbac8aad7
Felipe Merchán <felipe-sc@hotmail.es>
2020-07-14 00:15:44 -0500

    Modulo 7 - clase: Controlar estado de carga y error al registrar un usuario

--- src/Components/UserForm/styles.js
+++ src/Components/UserForm/styles.js
@@ -11,6 +11,9 @@ export const Input = styled.input`
   border-radius: 5px;
   padding: 10px 5px;
   margin-bottom: 10px;
+  &[disabled] {
+    opacity: .3;
+  }
 `
 
 export const Button = styled.button`
@@ -21,10 +24,17 @@ export const Button = styled.button`
   color: #ffffff;
   text-align: center;
   background: #b83b5e;
+  &[disabled] {
+    opacity: .3;
+  }
 `
 
 export const Title = styled.h2`
   font-size: 16px;
   font-weight: 500;
   padding: 10px 0;
+`
+export const Error = styled.span`
+  font-size: 14px;
+  color: red;
 `
